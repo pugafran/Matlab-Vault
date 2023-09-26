0dbac4e9d5a2ce902eb452ce64da6f750964b7b1
pugafran <franciscplgi36@gmail.com>
2023-09-26 22:36:16 +0200

    Pre-Sesión 01

---
+++ RubricaPLAB_1.m
@@ -0,0 +1,37 @@
+% Área para rellenar el nombre y apellidos
+% Por ejemplo:
+% Nombre='José Fernández Fernández';
+Nombre='Pon aquí tu nombre y apellidos sin borrar las comillas';
+
+disp(['Práctica entregada por ',Nombre]);
+%% Ejercicio 1
+disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
+disp('%%%%%%%%%%%%%%%%%%%%    Solución del ejercicio 1    %%%%%%%%%%%%%%%%%%%%');
+disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
+% Introduce aquí la solución del Ejercicio 1
+clear; % 
+
+
+disp('%%%%%%%%%%%%%%%%%%%%        Fin del Ejercicio 1          %%%%%%%%%%%%%%%%%%%%');
+
+%% Ejercicio 2
+disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
+disp('%%%%%%%%%%%%%%%%%%%%    Solución del ejercicio 2    %%%%%%%%%%%%%%%%%%%%');
+disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
+% Introduce aquí la solución del Ejercicio 2
+clear; % 
+
+
+disp('%%%%%%%%%%%%%%%%%%%%        Fin del Ejercicio 2          %%%%%%%%%%%%%%%%%%%%');
+
+%% Ejercicio 3
+disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
+disp('%%%%%%%%%%%%%%%%%%%%    Solución del ejercicio 3    %%%%%%%%%%%%%%%%%%%%');
+disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
+% Introduce aquí la solución del Ejercicio 3
+clear; % 
+
+
+disp('%%%%%%%%%%%%%%%%%%%%        Fin del Ejercicio 3          %%%%%%%%%%%%%%%%%%%%');
+
+
---
+++ RubricaPLAB_2.m
@@ -0,0 +1,24 @@
+% Área para rellenar el nombre y apellidos
+% Por ejemplo:
+% Nombre='José Fernández Fernández';
+Nombre='Pon aquí tu nombre y apellidos sin borrar las comillas';
+
+disp(['Práctica entregada por ',Nombre]);
+%% Ejercicio 13
+disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
+disp('%%%%%%%%%%%%%%%%%%%%    Solución del ejercicio 13    %%%%%%%%%%%%%%%%%%%%');
+disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
+% Introduce aquí la solución del Ejercicio 13
+clear; % 
+
+
+disp('%%%%%%%%%%%%%%%%%%%%        Fin del Ejercicio 13          %%%%%%%%%%%%%%%%%%%%');
+
+%% Ejercicio 14
+disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
+disp('%%%%%%%%%%%%%%%%%%%%    Solución del ejercicio 14    %%%%%%%%%%%%%%%%%%%%');
+disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
+% Introduce aquí la solución del Ejercicio 14
+clear; % 
+
+disp('%%%%%%%%%%%%%%%%%%%%        Fin del Ejercicio 14          %%%%%%%%%%%%%%%%%%%%');
